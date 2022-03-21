f0671bac8c7b7b7461f5c632c21aa86984edd4b1
gidiyan <gidiyan@gmail.com>
2022-03-21 13:21:18 +0200

    feat: added target host, colorized output

--- start.py
+++ start.py
@@ -34,7 +34,6 @@ from impacket.ImpactPacket import IP, TCP, UDP, Data
 from psutil import cpu_percent, net_io_counters, process_iter, virtual_memory
 from requests import Response, Session, exceptions, get, cookies
 from yarl import URL
-import re
 
 basicConfig(format='[%(asctime)s - %(levelname)s] %(message)s',
             datefmt="%H:%M:%S")
