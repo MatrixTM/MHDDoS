7bfd63fc663e46cf935c4b289f2f90697d75b083
Nexus <78029616+Nexuzzzz@users.noreply.github.com>
2022-07-02 00:04:41 +0000

    Add more tor2web gateways

--- start.py
+++ start.py
@@ -48,7 +48,43 @@ ctx.verify_mode = CERT_NONE
 __version__: str = "2.4 SNAPSHOT"
 __dir__: Path = Path(__file__).parent
 __ip__: Any = None
-tor2webs = ['onion.ly', 'tor2web.to', 'onion.org', 'onion.pet', 'onion.ws', 'onion.top', 'onion.dog']
+tor2webs = [
+            'onion.city',
+            'onion.cab',
+            'onion.direct',
+            'onion.sh',
+            'onion.link',
+            'onion.ws',
+            'onion.pet',
+            'onion.rip',
+            'onion.plus',
+            'onion.top',
+            'onion.si',
+            'onion.ly',
+            'onion.my',
+            'onion.sh',
+            'onion.lu',
+            'onion.casa',
+            'onion.com.de',
+            'onion.foundation',
+            'onion.rodeo',
+            'onion.lat',
+            'tor2web.org',
+            'tor2web.fi',
+            'tor2web.blutmagie.de',
+            'tor2web.to',
+            'tor2web.io',
+            'tor2web.in',
+            'tor2web.it',
+            'tor2web.xyz',
+            'tor2web.su',
+            'darknet.to',
+            's1.tor-gateways.de',
+            's2.tor-gateways.de',
+            's3.tor-gateways.de',
+            's4.tor-gateways.de',
+            's5.tor-gateways.de'
+        ]
 
 with open(__dir__ / "config.json") as f:
     con = load(f)
