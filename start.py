6d162050f07930b4007a6418abc8c9b08fa82d62
Kuchiriel <matheus.almeida211094@gmail.com>
2024-09-04 18:25:38 -0300

    Update start.py
    Fixed:
    
    MHDDoS/start.py:168: SyntaxWarning: invalid escape sequence '\d'
      IP = compile("(?:\d{1,3}\.){3}\d{1,3}")
    
    MHDDoS/start.py:169: SyntaxWarning: invalid escape sequence '\d'
      protocolRex = compile('"protocol":(\d+)')

--- start.py
+++ start.py
@@ -167,7 +167,7 @@ BYTES_SEND = Counter()
 class Tools:
     IP = compile(r"(?:\d{1,3}\.){3}\d{1,3}")
     protocolRex = compile(r'"protocol":(\d+)')
-
+ 
     @staticmethod
     def humanbytes(i: int, binary: bool = False, precision: int = 2):
         MULTIPLES = [
